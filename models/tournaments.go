aef263ef72c03b5f4ce6f7dbfc8bf29a055687fa
Lukasz Chmielewski <lu.chmielewski@gmail.com>
2022-09-08 13:06:02 +0200

    Revert "Fixed draw calculations"
    This reverts commit 5cc38d5ad5be5a57315aa2f1670f75354ecee2a4.

--- models/tournaments.go
+++ models/tournaments.go
@@ -4,19 +4,3 @@ type TournamentMetadata struct {
 	TournamentId int `json:"tournament_id"`
 	BetsOff      int `json:"bets_off"`
 }
-
-type TopGameResult struct {
-	UserId  int     `json:"user_id"`
-	MatchId int     `json:"match_id"`
-	Amount  float64 `json:"amount"`
-}
-
-type TournamentStatistics struct {
-	TournamentId        int             `json:"tournament_id"`
-	TournamentBetsCount int             `json:"tournament_bets_count"`
-	TournamentUserCount int             `json:"tournament_user_count"`
-	BankWinnings        float64         `json:"bank_winnings"`
-	UserWinnings        float64         `json:"user_winnings"`
-	BiggestWins         []TopGameResult `json:"biggest_wins"`
-	BiggestLosses       []TopGameResult `json:"biggest_losses"`
-}
